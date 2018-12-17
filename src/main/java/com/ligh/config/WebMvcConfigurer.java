bb8776f31d27afc85cf0b6d508097369daf4ba4f
ligh <liguohui@huluwa.cc>
2018-12-17 15:23:01 +0800

    ligh

--- src/main/java/com/ligh/config/WebMvcConfigurer.java
+++ src/main/java/com/ligh/config/WebMvcConfigurer.java
@@ -3,6 +3,7 @@ package com.ligh.config;
 import com.ligh.interceptor.OneInterceptor;
 import com.ligh.interceptor.TwoInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -10,10 +11,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 public class WebMvcConfigurer extends WebMvcConfigurationSupport{
 
     /**
+     *  处理前后端跨域问题
+     * @param registry
+     */
+//    @Override
+//    protected void addCorsMappings(CorsRegistry registry) {
+//        System.out.println("hahaha");
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .allowedOrigins("*");
+//    }
+
+    /**
      *  拦截器安装顺序执行
      *
      * @param registry
      */
+
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
 
---
+++ src/test/java/com/ligh/test/TestSpringbootEmail.java
@@ -0,0 +1,18 @@
+package com.ligh.test;
+
+import com.ligh.entity.Resource;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by ${ligh} on 2018/12/7 上午9:15
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSpringbootEmail {
+
+
+
+}
