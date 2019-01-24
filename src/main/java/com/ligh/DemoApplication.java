5157421842a764d6578d59338ead4457c6d5acba
liguohui <lighAsqh@163.com>
2019-01-24 16:31:22 +0800

    ligh

--- src/main/java/com/ligh/DemoApplication.java
+++ src/main/java/com/ligh/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.ligh;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
--- src/main/java/com/ligh/task/TestTask.java
+++ src/main/java/com/ligh/task/TestTask.java
@@ -13,10 +13,10 @@ public class TestTask {
 
     //定义没过3秒执行任务
 //    @Scheduled(fixedRate = 3000)
+//                        秒    分  时 日 月年   每天的6点,17点,的每16秒,36秒,46秒执行一次
     @Scheduled(cron = "16,36,46 0 6,17 * * ? ")
     public void reportCurrentTime(){
         System.out.println("现在时间:" +dateFormat.format(new Date()));
     }
-
 
 }
